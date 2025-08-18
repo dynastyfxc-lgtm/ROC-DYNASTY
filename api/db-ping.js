b4d0d4bcb6f3a047f3ae93b6cbc57f53155f0180
dynastyfxc-lgtm <dynastyfxc@gmail.com>
2025-08-18 01:18:08 -0400

    Update db-ping.js

--- api/db-ping.js
+++ api/db-ping.js
@@ -1,9 +1,9 @@
 // api/db-ping.js
-import { db } from "../lib/firebaseAdmin";
+import { db } from "../lib/firebaseAdmin.js";
 
 export default async function handler(req, res) {
   try {
-    // Try to read one document from a "test" collection
+    // Simple read to prove Firestore connectivity
     const snapshot = await db.collection("test").limit(1).get();
 
     res.status(200).json({
